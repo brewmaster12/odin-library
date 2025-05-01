e4eef62a14cd6f04787257a7b2ca0cc4bfddb34f
brewmaster12 <seanywg@gmail.com>
2025-05-01 15:42:16 +0100

    Remove 'new.target' safeguard

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ let myLibrary = [];
 
 class Book {
     constructor(title, author, pages, read) {
-        if (!new.target) {
-            throw Error("You must use the 'new' operator to call the constructor");
-        }
         this.title = title;
         this.author = author;
         this.pages = pages;
