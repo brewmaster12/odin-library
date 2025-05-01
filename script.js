1f653857bf1a7e2eeca4e5a7745a7714b4ba58b6
brewmaster12 <seanywg@gmail.com>
2025-05-01 15:31:30 +0100

    Change plain constructor to class constructor

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 let myLibrary = []; 
  
-function Book(title, author, pages, read) {
-    if (!new.target) {
-        throw Error("You must use the 'new' operator to call the constructor");
-    }
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.id = crypto.randomUUID();
-}
 
-Book.prototype.changeReadStatus = function() {
-    if (this.read === "read") {
-        this.read = "not read"
-    } else if (this.read === "not read") {
-        this.read = "read"
+class Book {
+    constructor(title, author, pages, read) {
+        if (!new.target) {
+            throw Error("You must use the 'new' operator to call the constructor");
+        }
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.id = crypto.randomUUID();
     }
-};
+
+    changeReadStatus() {
+        if (this.read === "read") {
+            this.read = "not read"
+        } else if (this.read === "not read") {
+            this.read = "read"
+        }
+    }
+}
 
 function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(new Book(title, author, pages, read));
